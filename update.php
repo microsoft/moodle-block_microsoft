3da88a4d4701b3051e943e4c53e59bad23ada98e
Charles Verge <charles.verge@remote-learner.net>
2015-12-18 17:28:32 -0400

    MSFTMPP-311: Option to Sync Office 365 profile pictures with Moodle profile pictures

---
+++ update.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package block_microsoft
+ * @author James McQuillan <james.mcquillan@remote-learner.net>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2015 onwards Microsoft Open Technologies, Inc. (http://msopentech.com/)
+ */
+
+require_once(__DIR__.'/../../config.php');
+require_login();
+$aadsync = get_config('local_o365', 'aadsync');
+$aadsync = array_flip(explode(',', $aadsync));
+// Only profile sync once for each session.
+if (empty($SESSION->block_microsoft_profilesync) && isset($aadsync['photosynconlogin'])) {
+    $PAGE->requires->jquery();
+    $usersync = new \local_o365\feature\usersync\main();
+    $usersync->assign_photo($USER->id, null);
+    $SESSION->block_microsoft_profilesync = true;
+}
